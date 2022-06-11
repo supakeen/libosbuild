aa573cc39dd8062e320c8911e578251512183457
Simon de Vlieger <cmdr@supakeen.com>
2022-06-11 13:58:57 +0200

    test: add another dummy test to test CI

--- src/lib.rs
+++ src/lib.rs
@@ -326,4 +326,10 @@ mod tests {
         let result = 2 + 2;
         assert_eq!(result, 4);
     }
+
+    #[test]
+    fn it_works_better() {
+        let result = 2 + 4;
+        assert_eq!(result, 6);
+    }
 }
