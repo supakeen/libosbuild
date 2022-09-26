3568b784d6b9cbc88d6d12377fa10c5e8c9acb6c
Simon de Vlieger <cmdr@supakeen.com>
2022-09-26 13:14:18 +0200

    fix typo

--- libosbuild/README.md
+++ libosbuild/README.md
@@ -2,3 +2,5 @@
 
 `libosbuild` is a Rust library that contains helpers to implement your own
 modules (stages, assemblers, sources) for [osbuild](https://osbuild.org/).
+
+## Terminology
--- libosbuild/src/manifest/description/mod.rs
+++ libosbuild/src/manifest/description/mod.rs
@@ -1,4 +1,4 @@
-/// Version 1 of the manifest description This is the first version of the osbuild manifest description,
+/// Version 1 of the manifest description. This is the first version of the osbuild manifest description,
 /// that has a main pipeline that consists of zero or more stages to create a tree and optionally one assembler that assembles
 /// the created tree into an artefact. The pipeline can have any number of nested build pipelines. A sources section is used
 /// to fetch resources.
