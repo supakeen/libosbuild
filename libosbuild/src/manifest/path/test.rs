cab5860b09bc2b248e00fdc0641e8f9e0a480fa0
Simon de Vlieger <cmdr@supakeen.com>
2022-09-28 11:22:47 +0200

    org: move path to own mod

--- libosbuild/src/manifest/path/test.rs
+++ libosbuild/src/manifest/path/test.rs
@@ -1,49 +1,6 @@
-use std::fmt;
-use std::ops;
-
-pub enum Part {
-    Name(String),
-    Index(usize),
-}
-
-pub struct Path(pub Vec<Part>);
-
-impl Path {
-    pub fn new(path: Vec<Part>) -> Self {
-        Self { 0: path }
-    }
-}
-
-impl ops::Deref for Path {
-    type Target = Vec<Part>;
-
-    fn deref(&self) -> &Self::Target {
-        &self.0
-    }
-}
-
-impl fmt::Display for Path {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        if self.is_empty() {
-            write!(f, ".")
-        } else {
-            self.iter().fold(Ok(()), |result, part| match part {
-                Part::Name(path) => {
-                    if path.contains(' ') {
-                        result.and_then(|_| write!(f, ".'{}'", path))
-                    } else {
-                        result.and_then(|_| write!(f, ".{}", path))
-                    }
-                }
-                Part::Index(path) => result.and_then(|_| write!(f, "[{}]", path)),
-            })
-        }
-    }
-}
-
 #[cfg(test)]
 pub mod test {
-    use super::*;
+    use crate::manifest::path::*;
 
     #[test]
     fn fmt_path() {
